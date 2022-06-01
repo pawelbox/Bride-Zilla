095ec909199f9848e156efd264b2610f4b9f8dd1
Paweł <pawel_box@wp.pl>
2022-06-01 12:23:15 +0200

    Add

---
+++ Bride Zilla/Koszty.Designer.cs
@@ -0,0 +1,332 @@
+﻿
+namespace Bride_Zilla
+{
+    partial class Koszty
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Koszty));
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.btnEdit = new System.Windows.Forms.Button();
+            this.groupBox4 = new System.Windows.Forms.GroupBox();
+            this.txtTravel = new System.Windows.Forms.TextBox();
+            this.label12 = new System.Windows.Forms.Label();
+            this.txtPrints = new System.Windows.Forms.TextBox();
+            this.label8 = new System.Windows.Forms.Label();
+            this.txtPaint = new System.Windows.Forms.TextBox();
+            this.label9 = new System.Windows.Forms.Label();
+            this.txtExtraPhoto = new System.Windows.Forms.TextBox();
+            this.label10 = new System.Windows.Forms.Label();
+            this.txtPhoto = new System.Windows.Forms.TextBox();
+            this.label11 = new System.Windows.Forms.Label();
+            this.groupBox2 = new System.Windows.Forms.GroupBox();
+            this.txtGroom = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.txtBride = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.txtID = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.txtPase = new System.Windows.Forms.TextBox();
+            this.label4 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.groupBox4.SuspendLayout();
+            this.groupBox2.SuspendLayout();
+            this.groupBox1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.AutoSizeColumnsMode = System.Windows.Forms.DataGridViewAutoSizeColumnsMode.Fill;
+            this.dataGridView1.AutoSizeRowsMode = System.Windows.Forms.DataGridViewAutoSizeRowsMode.AllCells;
+            this.dataGridView1.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Location = new System.Drawing.Point(12, 247);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.Size = new System.Drawing.Size(796, 457);
+            this.dataGridView1.TabIndex = 0;
+            // 
+            // btnEdit
+            // 
+            this.btnEdit.BackColor = System.Drawing.Color.White;
+            this.btnEdit.BackgroundImageLayout = System.Windows.Forms.ImageLayout.None;
+            this.btnEdit.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.btnEdit.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.btnEdit.Image = ((System.Drawing.Image)(resources.GetObject("btnEdit.Image")));
+            this.btnEdit.ImageAlign = System.Drawing.ContentAlignment.MiddleRight;
+            this.btnEdit.Location = new System.Drawing.Point(12, 710);
+            this.btnEdit.Name = "btnEdit";
+            this.btnEdit.Size = new System.Drawing.Size(127, 39);
+            this.btnEdit.TabIndex = 22;
+            this.btnEdit.Text = "Edytuj rachunek";
+            this.btnEdit.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.btnEdit.UseVisualStyleBackColor = false;
+            this.btnEdit.Click += new System.EventHandler(this.btnEdit_Click);
+            // 
+            // groupBox4
+            // 
+            this.groupBox4.Controls.Add(this.txtPase);
+            this.groupBox4.Controls.Add(this.label4);
+            this.groupBox4.Controls.Add(this.txtTravel);
+            this.groupBox4.Controls.Add(this.label12);
+            this.groupBox4.Controls.Add(this.txtPrints);
+            this.groupBox4.Controls.Add(this.label8);
+            this.groupBox4.Controls.Add(this.txtPaint);
+            this.groupBox4.Controls.Add(this.label9);
+            this.groupBox4.Controls.Add(this.txtExtraPhoto);
+            this.groupBox4.Controls.Add(this.label10);
+            this.groupBox4.Controls.Add(this.txtPhoto);
+            this.groupBox4.Controls.Add(this.label11);
+            this.groupBox4.Location = new System.Drawing.Point(388, 5);
+            this.groupBox4.Name = "groupBox4";
+            this.groupBox4.Size = new System.Drawing.Size(420, 224);
+            this.groupBox4.TabIndex = 25;
+            this.groupBox4.TabStop = false;
+            this.groupBox4.Text = "Dodatki";
+            // 
+            // txtTravel
+            // 
+            this.txtTravel.Location = new System.Drawing.Point(174, 78);
+            this.txtTravel.Name = "txtTravel";
+            this.txtTravel.Size = new System.Drawing.Size(153, 21);
+            this.txtTravel.TabIndex = 9;
+            // 
+            // label12
+            // 
+            this.label12.AutoSize = true;
+            this.label12.Location = new System.Drawing.Point(174, 62);
+            this.label12.Name = "label12";
+            this.label12.Size = new System.Drawing.Size(40, 13);
+            this.label12.TabIndex = 8;
+            this.label12.Text = "Dojazd";
+            // 
+            // txtPrints
+            // 
+            this.txtPrints.Location = new System.Drawing.Point(174, 37);
+            this.txtPrints.Name = "txtPrints";
+            this.txtPrints.Size = new System.Drawing.Size(153, 21);
+            this.txtPrints.TabIndex = 7;
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.Location = new System.Drawing.Point(174, 21);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(49, 13);
+            this.label8.TabIndex = 6;
+            this.label8.Text = "Wydruki";
+            // 
+            // txtPaint
+            // 
+            this.txtPaint.Location = new System.Drawing.Point(7, 119);
+            this.txtPaint.Name = "txtPaint";
+            this.txtPaint.Size = new System.Drawing.Size(153, 21);
+            this.txtPaint.TabIndex = 5;
+            // 
+            // label9
+            // 
+            this.label9.AutoSize = true;
+            this.label9.Location = new System.Drawing.Point(7, 103);
+            this.label9.Name = "label9";
+            this.label9.Size = new System.Drawing.Size(42, 13);
+            this.label9.TabIndex = 4;
+            this.label9.Text = "Obrazy";
+            // 
+            // txtExtraPhoto
+            // 
+            this.txtExtraPhoto.Location = new System.Drawing.Point(7, 79);
+            this.txtExtraPhoto.Name = "txtExtraPhoto";
+            this.txtExtraPhoto.Size = new System.Drawing.Size(153, 21);
+            this.txtExtraPhoto.TabIndex = 3;
+            // 
+            // label10
+            // 
+            this.label10.AutoSize = true;
+            this.label10.Location = new System.Drawing.Point(7, 62);
+            this.label10.Name = "label10";
+            this.label10.Size = new System.Drawing.Size(101, 13);
+            this.label10.TabIndex = 2;
+            this.label10.Text = "Albumy dodatkowe";
+            // 
+            // txtPhoto
+            // 
+            this.txtPhoto.Location = new System.Drawing.Point(7, 38);
+            this.txtPhoto.Name = "txtPhoto";
+            this.txtPhoto.Size = new System.Drawing.Size(153, 21);
+            this.txtPhoto.TabIndex = 1;
+            // 
+            // label11
+            // 
+            this.label11.AutoSize = true;
+            this.label11.Location = new System.Drawing.Point(7, 21);
+            this.label11.Name = "label11";
+            this.label11.Size = new System.Drawing.Size(38, 13);
+            this.label11.TabIndex = 0;
+            this.label11.Text = "Album";
+            // 
+            // groupBox2
+            // 
+            this.groupBox2.Controls.Add(this.txtGroom);
+            this.groupBox2.Controls.Add(this.label3);
+            this.groupBox2.Controls.Add(this.txtBride);
+            this.groupBox2.Controls.Add(this.label2);
+            this.groupBox2.Location = new System.Drawing.Point(12, 99);
+            this.groupBox2.Name = "groupBox2";
+            this.groupBox2.Size = new System.Drawing.Size(343, 130);
+            this.groupBox2.TabIndex = 24;
+            this.groupBox2.TabStop = false;
+            this.groupBox2.Text = "Para młoda";
+            // 
+            // txtGroom
+            // 
+            this.txtGroom.Location = new System.Drawing.Point(7, 79);
+            this.txtGroom.Name = "txtGroom";
+            this.txtGroom.Size = new System.Drawing.Size(295, 21);
+            this.txtGroom.TabIndex = 3;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(7, 62);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(58, 13);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "Pan młody";
+            // 
+            // txtBride
+            // 
+            this.txtBride.Location = new System.Drawing.Point(7, 38);
+            this.txtBride.Name = "txtBride";
+            this.txtBride.Size = new System.Drawing.Size(295, 21);
+            this.txtBride.TabIndex = 1;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(7, 21);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(68, 13);
+            this.label2.TabIndex = 0;
+            this.label2.Text = "Panna młoda";
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.txtID);
+            this.groupBox1.Controls.Add(this.label1);
+            this.groupBox1.Location = new System.Drawing.Point(12, 5);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(343, 88);
+            this.groupBox1.TabIndex = 23;
+            this.groupBox1.TabStop = false;
+            // 
+            // txtID
+            // 
+            this.txtID.Location = new System.Drawing.Point(7, 38);
+            this.txtID.Name = "txtID";
+            this.txtID.Size = new System.Drawing.Size(295, 21);
+            this.txtID.TabIndex = 1;
+            this.txtID.TextChanged += new System.EventHandler(this.txtID_TextChanged);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(7, 21);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(54, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "ID klienta";
+            // 
+            // txtPase
+            // 
+            this.txtPase.Location = new System.Drawing.Point(174, 119);
+            this.txtPase.Name = "txtPase";
+            this.txtPase.Size = new System.Drawing.Size(153, 21);
+            this.txtPase.TabIndex = 11;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(174, 103);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(49, 13);
+            this.label4.TabIndex = 10;
+            this.label4.Text = "Winietki";
+            // 
+            // Koszty
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.White;
+            this.ClientSize = new System.Drawing.Size(820, 761);
+            this.Controls.Add(this.groupBox4);
+            this.Controls.Add(this.groupBox2);
+            this.Controls.Add(this.groupBox1);
+            this.Controls.Add(this.btnEdit);
+            this.Controls.Add(this.dataGridView1);
+            this.Font = new System.Drawing.Font("Lato", 8.25F);
+            this.Name = "Koszty";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Koszty";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.groupBox4.ResumeLayout(false);
+            this.groupBox4.PerformLayout();
+            this.groupBox2.ResumeLayout(false);
+            this.groupBox2.PerformLayout();
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.Button btnEdit;
+        private System.Windows.Forms.GroupBox groupBox4;
+        public System.Windows.Forms.TextBox txtPase;
+        private System.Windows.Forms.Label label4;
+        public System.Windows.Forms.TextBox txtTravel;
+        private System.Windows.Forms.Label label12;
+        public System.Windows.Forms.TextBox txtPrints;
+        private System.Windows.Forms.Label label8;
+        public System.Windows.Forms.TextBox txtPaint;
+        private System.Windows.Forms.Label label9;
+        public System.Windows.Forms.TextBox txtExtraPhoto;
+        private System.Windows.Forms.Label label10;
+        public System.Windows.Forms.TextBox txtPhoto;
+        private System.Windows.Forms.Label label11;
+        private System.Windows.Forms.GroupBox groupBox2;
+        public System.Windows.Forms.TextBox txtGroom;
+        private System.Windows.Forms.Label label3;
+        public System.Windows.Forms.TextBox txtBride;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.GroupBox groupBox1;
+        public System.Windows.Forms.TextBox txtID;
+        private System.Windows.Forms.Label label1;
+    }
+}
---
+++ Bride Zilla/Koszty.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.Windows.Forms;
+
+namespace Bride_Zilla
+{
+    public partial class Koszty : Form
+    {
+        public void ClearText()
+        {
+            txtPhoto.Clear();
+            txtExtraPhoto.Clear();
+            txtPaint.Clear();
+            txtPrints.Clear();
+            txtPase.Clear();
+            txtTravel.Clear();
+            txtBride.Clear();
+            txtGroom.Clear();
+        }
+        public void ReloadGrid()
+        {
+            LoadData load = new LoadData();
+            load.LoadGridData("costs");
+            dataGridView1.DataSource = load.data.Tables[0];
+        }
+        public Koszty()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            ReloadGrid();
+        }
+
+        private void txtID_TextChanged(object sender, EventArgs e)
+        {
+            if (txtID.Text.Length >= 4)
+            {
+                ClearText();
+                string customerID = txtID.Text;
+                try
+                {
+                    LoadData load = new LoadData();
+                    load.LoadCostsData("costs", customerID);
+                    txtPhoto.AppendText(load.photobook.ToString());
+                    txtExtraPhoto.AppendText(load.extraPhotobook.ToString());
+                    txtPaint.AppendText(load.paint.ToString());
+                    txtPrints.AppendText(load.prints.ToString());
+                    txtPase.AppendText(load.pase.ToString());
+                    txtTravel.AppendText(load.travel.ToString());
+
+                    LoadData customer = new LoadData();
+                    customer.LoadCustomerData("customers", customerID);
+                    txtBride.AppendText(customer.bride);
+                    txtGroom.AppendText(customer.groom);
+                }
+                catch (Exception)
+                {
+                }
+            }
+        }
+
+        private void btnEdit_Click(object sender, EventArgs e)
+        {
+            DataOperations update = new DataOperations();
+            
+            update.ID = int.Parse(txtID.Text);
+            update.photobook = int.Parse(txtPhoto.Text);
+            update.extraPhotobook = int.Parse(txtExtraPhoto.Text);
+            update.paint = int.Parse(txtPaint.Text);
+            update.prints = int.Parse(txtPrints.Text);
+            update.pase = int.Parse(txtPase.Text);
+            update.travel = int.Parse(txtTravel.Text);
+            update.Update("costs", " SET `ID`=@ID,`Album`=@photobook,`Albumy dodatkowe`=@extraPhoto,`Obrazy`=@paint,`Wydruki`=@prints," +
+                "`Winietki`=@pase,`Paliwo`=@travel WHERE ID= @ID");
+            ClearText();
+            ReloadGrid();
+        }
+    }
+}
