c29ce929940b2a8cc3917ea00f8f830db54bb5a9
Paweł Klimkowski <103247653+pawelbox@users.noreply.github.com>
2022-06-01 11:50:51 +0200

    Add files via upload

---
+++ Bride Zilla/ConnectionString.cs
@@ -0,0 +1,22 @@
+﻿using MySqlConnector;
+using System;
+using MySql.Data.MySqlClient;
+
+namespace Bride_Zilla
+{
+    class ConnectionString
+    {
+        //Just add your MySql Connection data here
+        public string server = "sql70.lh.pl";
+        public string database = "**************";
+        public string uid = "******************";
+        public string pwd = "*************";
+        public MySql.Data.MySqlClient.MySqlConnection con { get; set; }
+        public void Connect()
+        {
+            ConnectionString connection = new ConnectionString();
+            con = new MySql.Data.MySqlClient.MySqlConnection("server=" + connection.server + ";user=" + connection.uid + ";database=" + connection.database +
+                    ";password=" + connection.pwd + ";");
+        }
+    }
+}
