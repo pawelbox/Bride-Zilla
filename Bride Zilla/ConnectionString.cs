0f230edcdb1e5c0650fd6dff97246eb4842a1f3d
Paweł Klimkowski <pawel_box@wp.pl>
2022-06-01 20:23:23 +0200

    Add

--- Bride Zilla/ConnectionString.cs
+++ Bride Zilla/ConnectionString.cs
@@ -8,9 +8,9 @@ namespace Bride_Zilla
     {
         //Just add your MySql Connection data here
         public string server = "sql70.lh.pl";
-        public string database = "*********";
-        public string uid = "**********";
-        public string pwd = "***********";
+        public string database = "serwer117140_customer";
+        public string uid = "serwer117140_customer";
+        public string pwd = "!Trudne021";
         public MySql.Data.MySqlClient.MySqlConnection con { get; set; }
         public void Connect()
         {
