407e7a11de15d1b15a8569ae74d70a09fe917090
Paweł <pawel_box@wp.pl>
2022-06-13 11:45:23 +0200

    ContentClick to class

---
+++ Bride Zilla/ContentClick.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Bride_Zilla
+{
+    class ContentClick
+    {
+        public string id { get; set; }
+
+        public void Click(object value)
+        {
+            value.ToString();
+            id = value.ToString();
+        }
+    }
+}
